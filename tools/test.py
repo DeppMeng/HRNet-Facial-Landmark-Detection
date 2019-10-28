22e89c354ef67da3468aaa0fbe0104b6390729e9
Depu Meng (FA Talent) <v-depme@microsoft.com>
2019-10-28 10:21:38 +0800

    configuring load model paths in test.py

--- tools/test.py
+++ tools/test.py
@@ -76,7 +76,8 @@ def main():
             final_output_dir, 'final_state.pth'
         )
         logger.info('=> loading model from {}'.format(model_state_file))
-        model.load_state_dict(torch.load(model_state_file).state_dict())
+        model_state = torch.load(model_state_file)
+        model.load_state_dict(model_state.state_dict())
  
 
     dataset_type = get_dataset(config)
